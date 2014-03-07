99a87b5f4fd3740c8d945f6518df610a8985d310
Kamil Prosciewicz <kprosciewicz@gmail.com>
2014-03-07 21:04:29 +0100

    ignore dla db config

--- admin/index.php
+++ admin/index.php
@@ -1,46 +1,52 @@
-﻿<html lang="pl">
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
+    <link   href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
 </head>
-<h2>Upload plików html. Zalecane jest używanie Google Chrome.</h2>
-<form action="index.php" method="post" enctype='multipart/form-data'>
-    Nazwa szkoly: <input type="text" name="nazwa"/><br>
-    Adres szkoly: <input type="text" name="adres"/><br>
-    Telefon: <input type="text" name="telefon"/><br>
-    Strona szkoly: <input type="text" name="strona"/><br>
-    E-mail szkoly: <input type="text" name="mail"/><br>
-    Plik (html) : <input type="file" name="file"/><br>
-    <input type="submit" value="Wyślij">
-</form>
+ 
+<body>
+    <div class="container">
+            <div class="row">
+                <h3>CRUD</h3>
+            </div>
+            <div class="row">
+            <p>
+                <a href="create.php" class="btn btn-success">Stwórz</a>
+            </p>
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Nazwa szkoly</th>
+                      <th>Adres szkoly</th>
+                      <th>Telefon</th>
+                      <th>Strona szkoly</th>
+                      <th>E-mail szkoly</th>
+                      <th>Plik (html)</th>
+                      <th>Action</th>                      
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php
+                   require_once("../php/dbConn.php");
+                   $sql = 'SELECT * FROM szkoly ORDER BY id DESC';
+                   foreach ($pdo->query($sql) as $row) {
+                            echo '<tr>';
+                            echo '<td>'. $row['nazwa'] . '</td>';
+                            echo '<td>'. $row['adres'] . '</td>';
+                            echo '<td>'. $row['telefon'] . '</td>';
+                            echo '<td>'. $row['link'] . '</td>';
+                            echo '<td>'. $row['mail'] . '</td>';
+                            echo '<td>'. $row['html'] . '</td>';
+                            echo '<td><a class="btn btn-success" href="edit.php?id='.$row['id'].'">Edytuj</button></td>';
+                            echo '</tr>';
+                   }
+                  ?>
+                  </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
+  </body>
 </html>
-
-<?php
-require_once "../php/dbConn.php";
-if(isset($_POST['nazwa']) && isset($_POST['adres']) && isset($_POST['adres']) && isset($_POST['strona']) && isset($_POST['mail'])){
-	
-	$nazwa=$_POST['nazwa'];
-	$adres=$_POST['adres'];
-	$telefon=$_POST['adres'];
-	$strona=$_POST['strona'];
-	$mail=$_POST['mail'];
-	$plik = $_FILES["file"];
-    if ($plik["error"] > 0)
-    {
-        echo "Error: " . $plik["error"] . "<br>";
-    }
-	else
-	{
-        echo "<br>Upload: " . $plik["name"] . "<br>";
-        echo "Type: " . $plik["type"] . "<br>";
-        echo "Size: " . ($plik["size"] / 1024) . " kB<br>";
-        echo "Stored in: " . $plik["tmp_name"];
-		if(!is_dir("../szkoly/"))
-		{
-			mkdir("../szkoly", 0777);
-		}
-		move_uploaded_file($plik['tmp_name'],'../szkoly/'.$plik['name']);
-		//$pdo->query("INSERT INTO `szkoly` (nazwa,html) VALUES ('".$nazwa."','".$plik['name']."')") or print mysql_error();
-        $pdo->query("INSERT INTO `szkoly` (nazwa, adres, telefon, mail, html, link) VALUES ('".$nazwa."', '".$adres."', '".$telefon."', '".$mail."', '".$plik["name"]."', '".$strona."')") or print mysql_error();
-
-    }
-}
