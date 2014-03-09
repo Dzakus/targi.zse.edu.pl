1bd3c2545280d8f6d25c5134e65c95eb91e95111
Cieniutko <adic212@gmail.com>
2014-03-09 16:59:26 +0100

    Obraz (header)

--- targi.sql
+++ targi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas wygenerowania: 09 Mar 2014, 04:46
--- Wersja serwera: 5.5.32
--- Wersja PHP: 5.4.19
+-- Generation Time: 09 Mar 2014, 15:54
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Baza danych: `targi`
+-- Database: `targi`
 --
-CREATE DATABASE IF NOT EXISTS `targi` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
-USE `targi`;
 
 -- --------------------------------------------------------
 
@@ -28,20 +26,20 @@ USE `targi`;
 -- Struktura tabeli dla tabeli `admins`
 --
 
-DROP TABLE IF EXISTS `admins`;
 CREATE TABLE IF NOT EXISTS `admins` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `mail` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `pass` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=3 ;
 
 --
 -- Zrzut danych tabeli `admins`
 --
 
 INSERT INTO `admins` (`id`, `mail`, `pass`) VALUES
-(1, 'kprosciewicz@gmail.com', 'ab239c5a26a103f02214f1ae199f6dad0108e000');
+(1, 'kprosciewicz@gmail.com', '1c9cc7e88f0661de83382c35e8305a7177fa6219'),
+(2, 'adrian.cieniuch@gmail.com', 'e42b31d4bb2a1c6edfbb65cd6e6c60f8065493d8');
 
 -- --------------------------------------------------------
 
@@ -49,7 +47,6 @@ INSERT INTO `admins` (`id`, `mail`, `pass`) VALUES
 -- Struktura tabeli dla tabeli `szkoly`
 --
 
-DROP TABLE IF EXISTS `szkoly`;
 CREATE TABLE IF NOT EXISTS `szkoly` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nazwa` varchar(100) COLLATE utf8_polish_ci DEFAULT NULL,
@@ -60,14 +57,14 @@ CREATE TABLE IF NOT EXISTS `szkoly` (
   `link` varchar(255) COLLATE utf8_polish_ci NOT NULL,
   `views` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=2 ;
 
 --
 -- Zrzut danych tabeli `szkoly`
 --
 
 INSERT INTO `szkoly` (`id`, `nazwa`, `adres`, `telefon`, `mail`, `html`, `link`, `views`) VALUES
-(1, 'I LICEUM OGÓLNOKSZTAŁCĄCE im. Walentego Roździeńskiego w Zespole Szkół Ogólnokształcących Nr 1 ', 'Staszica 62 41-200 Sosnowiec', '(32) 266-18-34', 'lo1@sosnowiec.edu.pl', '1lo.htm', 'http://www.rozdzienski.pl', 1);
+(1, 'I LICEUM OGÓLNOKSZTAŁCĄCE im. Walentego Roździeńskiego w Zespole Szkół Ogólnokształcących Nr 1 ', 'Staszica 62 41-200 Sosnowiec', '(32) 266-18-34', 'lo1@sosnowiec.edu.pl', '1lo.htm', 'http://www.rozdzienski.pl', 3);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
