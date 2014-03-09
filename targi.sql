009789f6a35351d5f29316489fcda936f8b4581d
Kamil Prosciewicz <kprosciewicz@gmail.com>
2014-03-09 04:29:42 +0100

    Plik bazy

--- targi.sql
+++ targi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas wygenerowania: 08 Mar 2014, 22:13
+-- Czas wygenerowania: 09 Mar 2014, 04:28
 -- Wersja serwera: 5.5.32
 -- Wersja PHP: 5.4.19
 
@@ -19,6 +19,28 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `targi`
 --
+CREATE DATABASE IF NOT EXISTS `targi` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+USE `targi`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `admins`
+--
+
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mail` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `pass` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=2 ;
+
+--
+-- Zrzut danych tabeli `admins`
+--
+
+INSERT INTO `admins` (`id`, `mail`, `pass`) VALUES
+(1, 'kprosciewicz@gmail.com', '1c9cc7e88f0661de83382c35e8305a7177fa6219');
 
 -- --------------------------------------------------------
 
@@ -26,7 +48,6 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `szkoly`
 --
 
-DROP TABLE IF EXISTS `szkoly`;
 CREATE TABLE IF NOT EXISTS `szkoly` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nazwa` varchar(100) COLLATE utf8_polish_ci DEFAULT NULL,
@@ -44,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `szkoly` (
 --
 
 INSERT INTO `szkoly` (`id`, `nazwa`, `adres`, `telefon`, `mail`, `html`, `link`, `views`) VALUES
-(16, 'I LICEUM OGÓLNOKSZTAŁCĄCE im. Walentego Roździeńskiego w Zespole Szkół Ogólnokształcących Nr 1 ', 'Staszica 62 41-200 Sosnowiec', 'Staszica 62 41-', 'lo1@sosnowiec.edu.pl', '1lo.htm', '', 0);
+(16, 'I LICEUM OGÓLNOKSZTAŁCĄCE im. Walentego Roździeńskiego w Zespole Szkół Ogólnokształcących Nr 1 ', 'Staszica 62 41-200 Sosnowiec', '(32) 142-12-12', 'lo1@sosnowiec.edu.pl', '1lo.htm', 'http://www.zse.edu.pl', 20);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
