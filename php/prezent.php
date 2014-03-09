a5a9e99167340db03034d298bc41795d25f221ba
Cieniutko <adic212@gmail.com>
2014-03-09 13:35:04 +0100

    Zmiana kolorów w menu

--- php/prezent.php
+++ php/prezent.php
@@ -2,7 +2,7 @@
     require_once "php/dbConn.php";
     $szkoly=$pdo->query('SELECT * FROM szkoly');
     echo '<div class = "ramka span9">
-    <h2>Wybór szkoły</h2>
+    <h3>Prezentacje szkół</h3>
     <ul>';
     foreach($szkoly->fetchAll() as $sql){
         echo ' <li><a href="'.$sql["link"].'">'.$sql["nazwa"].'</a></li>';
